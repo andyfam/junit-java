c57807549e4df75b07a3eda0640a80e52367276e
andyfam <andyfam@gmail.com>
2024-06-03 17:17:30 -0600

    add some new features in Junit5

--- src/test/java/com/yufeng/junitjava/MyJunitTest.java
+++ src/test/java/com/yufeng/junitjava/MyJunitTest.java
@@ -1,10 +1,15 @@
 package com.yufeng.junitjava;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 class MyJunitTest {
+
+    static Logger log = Logger.getLogger(MyJunitTest.class.getName());
 
     @Test
     void sum() {
@@ -30,5 +35,50 @@ class MyJunitTest {
         assertEquals("C", myJunit.gradeLetter(70));
         assertEquals("B", myJunit.gradeLetter(80));
         assertEquals("A", myJunit.gradeLetter(90));
+
+        //exception test
+        assertThrows(IllegalArgumentException.class, () -> {
+            myJunit.gradeLetter(-1);
+        });
+    }
+
+    @BeforeAll
+    static void setup() {
+        log.info("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        log.info("@BeforeEach - executes before each test method in this class");
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("@AfterEach - executed after each test method.");
+    }
+
+    @AfterAll
+    static void done() {
+        log.info("@AfterAll - executed after all test methods.");
+    }
+
+    @DisplayName("Single test successful")
+    @Test
+    void testSingleSuccessTest() {
+        log.info("Success");
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void testShowSomething() {
+    }
+
+    @Test
+    void assumptionThat() {
+        String someString = "Just a string";
+        assumingThat(
+                someString.equals("Just a string"),
+                () -> assertEquals(2 + 2, 4)
+        );
     }
 }
